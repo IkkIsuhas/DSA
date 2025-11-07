f8d7da93978300c2fbae61b199eee40e9f14c7db
IkkIsuhas <suhasm004@gmail.com>
2025-11-07 23:04:04 +0530

    prog1

---
+++ Star3x3.java
@@ -0,0 +1,10 @@
+class Star3x3{
+    public static void main(String args[]){
+        for(int i=1;i<=3;i++){
+            for(int j=i;j<=3;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
